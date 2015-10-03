06431f37d518bbc48f6c5f513c0770887a62b1a5
Jacob Hacker <jacobhacker@outlook.com>
2015-10-03 16:26:23 -0500

    add ssh scripts & pub key

---
+++ main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/JacobHacker/new-server-setup/master/ssh.sh -O - | bash
---
+++ ssh.sh
@@ -0,0 +1,2 @@
+# TODO: create ssh dirs if not exists
+echo $(curl https://raw.githubusercontent.com/JacobHacker/new-server-setup/master/t430.pub -s) >> ~/.ssh/known_hosts
